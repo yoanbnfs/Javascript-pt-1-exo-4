45ed028b3cf8cbcad8c7d4e9be3826ca97efba9b
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-10-25 08:51:48 +0200

    exo4

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var yourLastname = prompt('Quel est votre nom ?');
+var yourFirstname = prompt('Quel est votre prénom');
+var yourCity = prompt('Quelle est votre ville de résidence ?');
+alert('Nom : ' + yourLastname + '\n' + 'prénom : ' + yourFirstname + '\n' + 'ville : ' + yourCity);
